dcc97164cf9c3ae85bc35a337abed431cbab60ec
Thura Freelance <mozet89@gmail.com>
2023-11-20 17:01:19 +0630

    add

--- src/components/App.js
+++ src/components/App.js
@@ -12,3 +12,36 @@ const App = () => {
 };
 
 export default App;
+Responsible for open and close work shop
+Perform data entry ,issuing invoice , answering ph call
+and assit customer request .
+Quotation of spare part and explatin to customer .
+Check all the spare part of qualilty,Price and compare
+with various of suppliers.
+Before the car go out make sure payment and all the
+car repair done .
+
+Handle with petty cash and do settlement of daily
+sales . Arrange all the payment and check with all
+the customer and suppliers .
+
+Customer Service Officer
+Company Name - Gandamar Shwe Pyi Logistic Pte,Ltd
+Role- Customer Service Officer and Procerment Officer
+Handle customer shipping document and check all the
+shipping date . Prepare all the document and check all the
+licene . Data entry and report to customer about shipping
+Try to solve all the problem for unexpected of shipping delay
+Issuse invoice for customer and inform to finance
+department.
+Perform computer dignostic and do some simple
+for Activation.
+
+Deal with all the price and sample for quality with china
+supplier .
+Daily check with all the customer and supplier for detail.
+
+Resolve product or service problems by clarifying the
+customer complaint,determing the cause of the
+problem,selecting and explaining the best solution to
+sovle the problem
